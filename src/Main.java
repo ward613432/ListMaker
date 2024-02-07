0213e680b21f9474e2ddcf4a87b3e43f0be43ff2
dado-offbrand <dogeychicken47@gmail.com>
2024-02-07 09:25:50 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<String> objects = new ArrayList<String>();
+        Scanner scanner = new Scanner(System.in);
+        boolean cont = true;
+
+        objects.add("c#");
+        objects.add("html");
+        objects.add("css");
+        objects.add("c++");
+        objects.add("java");
+        objects.add("python");
+        objects.add("swift");
+        objects.add("rust");
+        objects.add("f#");
+        objects.add("c");
+
+        while (cont) {
+            String input = InputHelper.getNonZeroLenString(scanner, "Options: A (add to list), D (delete from list), P (print the list), Q (quit the program)");
+
+            // all of the methods were redundant so I removed them
+            if (input.equalsIgnoreCase("a")) {
+                objects.add(InputHelper.getNonZeroLenString(scanner, "What would you like to add?"));
+            } else if (input.equalsIgnoreCase("d")) {
+                objects.remove(InputHelper.getRangedInt(scanner, "What is the object's index?", 1, objects.size()));
+            } else if (input.equalsIgnoreCase("p")) {
+                for (String o: objects) {
+                    System.out.printf("%d - %s\n", objects.indexOf(o), o);
+                }
+            } else if (input.equalsIgnoreCase("q")) {
+                if (InputHelper.getYNConfirm(scanner, "Are you sure you want to quit? [Y/N]")) {
+                    cont = false;
+                }
+            } else {
+                System.out.println("That was not an option.");
+            }
+        }
+    }
+}
